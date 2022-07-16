67aa617a449d24c870de51cd15ae34446272ca4c
Austin <austinmeyer1106@gmail.com>
2022-07-16 10:50:00 -0500

    made changes to html, js, and css file

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -7,3 +7,38 @@ In this file, you will also include the event listeners that are needed to inter
 a user clicks a button or adds a guess to the input field.
 
 */
+
+const input = document.getElementsByTagName
+('input')[0];
+
+console.log(input)
+
+const submitButton = document.getElementById
+('submit-button');
+
+const listItems = document.getElementsByTagName
+('li');
+
+let listItemIdx = 0;
+
+let winningNumber = Math.floor((Math.random() * 100) + 1)
+
+submitButton.addEventListener('click', function
+(event) {
+    // event.preventDefault keeps page from refreshing
+    event.preventDefault();
+
+    // event.target gives access to the target node where the event occurred
+
+    // event.value will give us the current value of that element (looking at the input element = input.value)
+    listItems[listItemIdx].innerText = input.value;
+
+    listItemIdx++;
+
+    const guess = parseInt(input.value)
+    if(winningNumber === guess){
+        console.log('you win!')
+    }
+});
+
+
