2eff84b89d38ceaf477e862b4f93e86e3d87b329
Austin <austinmeyer1106@gmail.com>
2022-07-18 11:37:11 -0500

    added additional functionality in js

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -1,13 +1,3 @@
-/* 
-
-Write your guess-game code here! Don't forget to look at the test specs as a guide. You can run the specs
-by running "npm test".
-
-In this file, you will also include the event listeners that are needed to interact with your HTML file when
-a user clicks a button or adds a guess to the input field.
-
-*/
-
 const input = document.getElementsByTagName
 ('input')[0];
 
@@ -19,26 +9,117 @@ const submitButton = document.getElementById
 const listItems = document.getElementsByTagName
 ('li');
 
+const guessHint = document.getElementById
+('guess-hint');
+
 let listItemIdx = 0;
 
-let winningNumber = Math.floor((Math.random() * 100) + 1)
+let winningNumber = 50;
+
+// let winningNumber = Math.floor((Math.random() * 100) + 1);
+
 
 submitButton.addEventListener('click', function
 (event) {
     // event.preventDefault keeps page from refreshing
     event.preventDefault();
-
+    
     // event.target gives access to the target node where the event occurred
-
+    
     // event.value will give us the current value of that element (looking at the input element = input.value)
     listItems[listItemIdx].innerText = input.value;
-
+    
     listItemIdx++;
-
+    
     const guess = parseInt(input.value)
+  
     if(winningNumber === guess){
-        console.log('you win!')
-    } 
+        guessHint.innerText = 'You win!'
+    }
+
+    if(listItemIdx === 5 && guess !== winningNumber){
+        guessHint.innerHTML = 'GAME OVER'
+    }
+    
+    if(Math.abs(winningNumber - guess) >= 30 && guess > winningNumber && guess != winningNumber && listItemIdx !== 5){
+        guessHint.innerText = "You're ice cold!"
+    }
+
+    if(Math.abs(winningNumber - guess) >= 30 && guess < winningNumber && guess != winningNumber && listItemIdx !== 5){
+        guessHint.innerText = "You're ice cold! Guess higher"
+    }
+
+    if(Math.abs(winningNumber - guess) > 10 && guess > winningNumber && guess != winningNumber && listItemIdx !== 5){
+        guessHint.innerText = "You're chilly. Guess lower"
+    }
+
+    if(Math.abs(winningNumber - guess) > 10 && guess < winningNumber && guess != winningNumber && listItemIdx !== 5){
+        guessHint.innerText = "You're chilly. Guess higher"
+    }
+
+    if(Math.abs(winningNumber - guess) <= 10 && guess > winningNumber && guess != winningNumber && listItemIdx !== 5){
+        guessHint.innerText = "You're warm. Guess lower"
+    }
+
+    if(Math.abs(winningNumber - guess) <= 10 && guess < winningNumber && guess != winningNumber && listItemIdx !== 5){
+        guessHint.innerText = "You're warm. Guess higher"
+    }
+
+    if(Math.abs(winningNumber - guess) <= 5 && guess > winningNumber && guess != winningNumber && listItemIdx !== 5){
+        guessHint.innerText = "You're scorching! Guess lower"
+    }
+
+    if(Math.abs(winningNumber - guess) <= 5 && guess < winningNumber && guess != winningNumber && listItemIdx !== 5){
+        guessHint.innerText = "You're scorching! Guess higher"
+    }
+
+    if(input.value.length === 0){
+        alert("Enter a number");
+        listItemIdx--;
+    }
+
+    if(typeOfinput.value === 0){
+        alert("Enter a number");
+        listItemIdx--;
+    }
+
+
 });
+
+
+
+// function generateWinningNumber(){
+//     return winningNumber;
+// }
+
+// function shuffle(array) {
+    //     let currentIndex = array.length,  randomIndex;
+  
+//     while (currentIndex != 0) {
+//       randomIndex = Math.floor(Math.random() * currentIndex);
+//       currentIndex--;
+      
+//       [array[currentIndex], array[randomIndex]] = [
+//         array[randomIndex], array[currentIndex]];
+//     }
+  
+//     return array;
+// }
+
+// function newGame(){
+//     let currentGuess = 
+//     const newGame = {
+//       playerGuess: 1,
+//       pastGuesses: 1,
+//    };
+//  return newGame
+// }
+
+
+
+
+
+
+
 
 
